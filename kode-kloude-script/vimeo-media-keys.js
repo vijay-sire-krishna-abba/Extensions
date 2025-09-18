decb1aeb84b4fda1ebe83f97547f626dbd3cd9d7
VijayKrishnaa-Abba <abbavijaykrishnaa@gmail.com>
2025-09-18 18:20:09 +0530

    removed hover for captions

--- kode-kloude-script/vimeo-media-keys.js
+++ kode-kloude-script/vimeo-media-keys.js
@@ -52,17 +52,7 @@ const controllerDivQuery = ".ControlBarV1_module_controlBarWrapper__cf14f860";
           const subtitleSpan = capDiv.querySelector("span");
           if (subtitleSpan) {
             subtitleSpan.style.setProperty("font-size", "14px", "important");
-            // subtitleSpan.style.setProperty("opacity", "0.1", "important");
-            // default opacity
-            subtitleSpan.style.setProperty("opacity", "0", "important");
-            // on hover
-            subtitleSpan.addEventListener("mouseenter", () => {
-              subtitleSpan.style.setProperty("opacity", "1", "important");
-            });
-            // back to normal when mouse leaves
-            subtitleSpan.addEventListener("mouseleave", () => {
-              subtitleSpan.style.setProperty("opacity", "0", "important");
-            });
+            subtitleSpan.style.setProperty("opacity", "0.2", "important");
 
             console.log("📝 Subtitle font resized.");
           }
